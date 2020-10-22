035e2f9b123007a0cd582a91a3d37153025b8191
Yannick De Decker <71879638+YannickDeDecker@users.noreply.github.com>
2020-10-22 09:54:11 +0200

    update

---
+++ 22 oktober 2020/script.js
@@ -0,0 +1,59 @@
+// JAVASCRIPT variables
+
+//VAR variable
+
+var intecbrussel = 1;
+intecbrussel = "hello";
+var intecbrussel = true;
+
+var _intecbrussel = "this is a special character"
+var string = "this is a string"
+
+console.log(intecbrussel)
+console.log(string)
+
+//reserved KEYWORDS
+
+//LET variable
+//cannot be redeclared, but can be updated
+
+let number = 40;
+number = 6;
+
+//CONST variabel
+//cannot be redeclared or updated, read-only, static variable
+
+const num = 50;
+
+//javascript OPERATORS
+
+var number1 = 40;
+var number2 = 34;
+
+var result = number1 + number2;
+var result = number1 - number2;
+var result = number1 * number2;
+var result = number1 / number2;
+var result = number1 % number2;
+
+//INCREMENT 1
+var result = number1++;
+
+//DECREMENT 1
+var result = number1--;
+
+//OPERATORS and strings
+var string1 = "hello,";
+var string2 = "is it me your looking for?"
+
+var result = string1 + " " + string2;
+
+console.log(result)
+
+//Assignment OPERATORS
+
+//x+=y means result = x + y
+var x = 5;
+var y = 8;
+var result = x+=y;
+console.log(result);
