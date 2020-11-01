792943da6f4b1203d25143820c727827ff68975b
Yannick De Decker <yannick@iMac.local>
2020-11-01 14:09:35 +0100

    update

---
+++ Javascript/Number Guessing Game/script.js
@@ -0,0 +1,25 @@
+let maximum = parseInt(prompt('Enter the maximum number!'));
+while (!maximum) {
+    maximum = parseInt(prompt('Enter the maximum number!'));
+}
+
+const targetNum =  Math.floor(Math.random() * maximum) + 1;
+
+let guess = parseInt(prompt('Enter your first guess.'));
+let attempts = 1;
+
+while (parseInt(guess) !== targetNum){
+    if (guess === 'q') break;
+    attempts++;
+    if(guess > targetNum){
+        guess = prompt('Too high! Enter a new guess.');
+    } else{
+        guess = prompt('Too low! Enter a new guess.');
+    }
+}
+
+if (guess === 'q'){
+    console.log('OK, you are a quitter!')
+} else {
+    console.log(`You got it! It took you ${attempts} guesses!`);
+}
