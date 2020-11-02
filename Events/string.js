1fba2164194ca935a15683087009901288f5f9ee
Yannick De Decker <yannick@iMac.local>
2020-11-02 14:04:35 +0100

    update

---
+++ Events/string.js
@@ -0,0 +1,9 @@
+function upperCase(){
+    let element = document.getElementById("upper");
+    element.innerText = element.innerText.toUpperCase();
+}
+
+function lowerCase(){
+    let element = document.getElementById("upper");
+    element.innerText = element.innerText.toLowerCase();
+}
