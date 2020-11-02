9a229d31dc380d1b5336f6aae127c9a3c11f8a2d
Yannick De Decker <yannick@iMac.local>
2020-11-02 10:32:25 +0100

    update

---
+++ Events/events.js
@@ -0,0 +1,24 @@
+// EVENTS
+
+function whenPageLoadEvent(){
+    alert('test');
+}
+
+function clickHandler(){
+    console.log('on click event');
+}
+
+function whenMouseOverFunction(){
+    console.log('Event type is whenMouseOverFunctio')
+}
+
+function whenMouseOutFunction(){
+    console.log('Event type is whenMouseOutFunctio')
+}
+
+
+mouseUpHandler
+mouseDownHandler
+oncopy
+ondblclick
+onkeydown
---
+++ Events/objects.js
@@ -0,0 +1,17 @@
+// STRINGS
+
+let fullName = new String('Jack Sparrow');
+console.log(fullName[0]);
+
+fullName.length //Print length of the string
+fulleName.indexOf('a') //Find the index of the first time a character appears
+
+// NUMBERS
+
+let salary = new Number(3000);
+console.log(salary);
+
+// BOOLEAN
+
+ let yes = new Boolean(true);
+ console.log(yes);
