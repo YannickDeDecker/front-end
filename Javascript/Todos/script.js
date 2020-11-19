5b87e8f0c7a90c88d660dc170832d749c7a70527
Yannick De Decker <71879638+YannickDeDecker@users.noreply.github.com>
2020-11-19 09:12:18 +0100

    update

--- Javascript/Todos/script.js
+++ Javascript/Todos/script.js
@@ -1,3 +1,5 @@
+
+
 // let todosOdd = document.querySelectorAll('.list-group-item:nth-of-type(odd)');
 // let todosEven = document.querySelectorAll('.list-group-item:nth-of-type(even)');
 
@@ -9,24 +11,42 @@
 //     list.classList.add('bg-secondary');
 // })
 
-let todos = document.querySelectorAll('.list-group-item');
 
-// todos.forEach((todo,index) => todo.classList.add(index % 2 === 0 ? 'bg-warning': 'bg-secondary'))
+function styleFunction(){
+    let todos = document.querySelectorAll('.list-group-item');
+    todos.forEach(function(todo,index){
+        if(index % 2 === 0){
+            todo.classList.add('bg-warning')
+        } else {
+            todo.classList.add('bg-secondary')
+            todo.classList.add('text-white')
+        }
+    })
+    let closeButton = document.querySelectorAll('.fa-remove');
 
-todos.forEach(function(todo,index){
-    if(index % 2 === 0){
-        todo.classList.add('bg-warning')
-    } else {
-        todo.classList.add('bg-secondary')
-        todo.classList.add('text-white')
-    }
-})
-
-let closeButton = document.querySelectorAll('.fa-remove');
-
-closeButton.forEach(function(button){
+    closeButton.forEach(function(button){
     button.classList.add('text-white')
 })
+}
+
+function addTodo(event){
+    event.preventDefault()
+    let enterTodo = document.querySelector('#todo').value;
+    let newTodo = document.createElement('li');
+    newTodo.innerHTML = `${enterTodo}
+    <a href="#" class="delete-item">
+        <i class="fa fa-remove"></i>
+    </a>`
+    newTodo.classList.add('list-group-item', 'd-flex', 'justify-content-between')
+    let todoList = document.querySelector('.list-group');
+    todoList.appendChild(newTodo);
+    styleFunction();
+}
+
+styleFunction();
+document.querySelector('#addTodo').addEventListener('click', addTodo);
+
+// todos.forEach((todo,index) => todo.classList.add(index % 2 === 0 ? 'bg-warning': 'bg-secondary'))
 
 // for (let i = 0; i < todos.length; i++){
 //     if(i % 2 === 0){
@@ -36,12 +56,3 @@ closeButton.forEach(function(button){
 //     }
 // }
 
-
-
-function addTodo(){
-    let enterTodo = document.querySelector('#todo').value;
-    let newTodo = document.createElement('li').innerText = enterTodo;
-    document.querySelector('.list-group').appendChild(newTodo);
-}
-
-document.querySelector('button[type]').addEventListener('click', addTodo);
