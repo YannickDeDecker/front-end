e6f53fc4bd753980f24cfdaf08081041c4deee98
Yannick De Decker <71879638+YannickDeDecker@users.noreply.github.com>
2020-11-27 11:15:56 +0100

    update

--- Javascript/Todos2/script.js
+++ Javascript/Todos2/script.js
@@ -1,14 +1,36 @@
-let listTodos = document.querySelectorAll('li');
+let inputTodo = document.querySelector('#todo');
+let buttonAddTodo = document.querySelector('#addTodo');
+const todoArray = JSON.parse(window.localStorage.getItem('ToDoList') || '[]');
 
-function filterTodos(){
-    let inputText = document.querySelector('#filter').value.toLowerCase();
-    listTodos.forEach(function(element){
-        if (inputText && element.innerText.toLowerCase().includes(inputText)){
-            element.classList.add('bg-warning')
-        } else {
-            element.classList.remove('bg-warning')
-        }
-    });
+function appendTodo(item) {
+    let template = document.querySelector("#todoTemplate").content.cloneNode(true);
+    template.querySelector('span').textContent = item
+    document.querySelector('#todoList').appendChild(template);
 }
 
-document.querySelector('#filter').addEventListener('input', filterTodos);
+function addTodo(event){
+    event.preventDefault();
+    let inputTodoValue = inputTodo.value;
+    todoArray.push(inputTodoValue);
+    window.localStorage.setItem('ToDoList', JSON.stringify(todoArray));
+    inputTodo.value = '';
+    appendTodo(inputTodoValue);
+}
+
+buttonAddTodo.addEventListener('click', addTodo);
+todoArray.forEach(appendTodo);
+
+let listTodos = document.querySelectorAll('li');
+
+// function filterTodos(){
+//     let inputText = document.querySelector('#filter').value.toLowerCase();
+//     listTodos.forEach(function(element){
+//         if (inputText && element.innerText.toLowerCase().includes(inputText)){
+//             element.classList.add('bg-warning')
+//         } else {
+//             element.classList.remove('bg-warning')
+//         }
+//     });
+// }
+
+// document.querySelector('#filter').addEventListener('input', filterTodos);
