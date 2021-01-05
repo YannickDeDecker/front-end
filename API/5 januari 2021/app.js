23ab09dcbc6275f14062b404321f8ee3fb2deff5
Yannick De Decker <71879638+YannickDeDecker@users.noreply.github.com>
2021-01-05 10:00:25 +0100

    update

---
+++ API/5 januari 2021/app.js
@@ -0,0 +1,8 @@
+const apiUrl= "http://localhost:3000/users";
+
+fetch(apiUrl)
+.then(response => response.json())
+.then(data => data.filter(function(user){
+    return user.address.city === 'McKenziehaven'
+}))
+.then(data => console.log(data))
