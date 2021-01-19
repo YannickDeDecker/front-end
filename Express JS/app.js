b5043cbd24d923a33ac106bb96117436c627998d
Yannick De Decker <71879638+YannickDeDecker@users.noreply.github.com>
2021-01-19 14:17:21 +0100

    update

--- Express JS/app.js
+++ Express JS/app.js
@@ -2,9 +2,36 @@
 
 const express = require('express');
 const app = express();
+const path = require('path');
+const members = require('./members');
+const moment = require('moment');
 
-//Initialize web server with express framework
+//CREATING MIDDLEWARE FUNCTION
+const logger = (req,res,next) => {
+    console.log('Hello World!');
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}: ${moment().format()}`);
+    next();
+}
 
-app.listen(3002, () => {
-    console.log('Server is running');
+app.use(logger) //initializing middleware function
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () =>{
+    console.log(`Server started on PORT ${PORT}`);
+});
+
+// app.get('/', (req,res) => {
+//     res.send('<h1>Hello World!</h1><h2>This is my first server</h2>');
+// });
+
+// app.get('/', (req,res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+//SET STATIC FOLDER
+app.use(express.static(path.join(__dirname, 'public')));
+
+//ROUTE DEFINITION
+app.get('/api/members', (req,res) =>{
+    res.json(members);
 })
---
+++ Express JS/members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'Yannick De Decker',
+        age: 28,
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Erik De Weerdt',
+        age: 34,
+        status: 'active'
+    },
+    {
+        id: 3,
+        name: 'Kenneth Devloo',
+        age: 30,
+        status: 'inactive'
+    }
+];
+
+module.exports = members;
