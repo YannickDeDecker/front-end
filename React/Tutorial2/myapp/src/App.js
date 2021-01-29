60228a4b040925585f89e4eb04a7112d2152708c
Yannick De Decker <71879638+YannickDeDecker@users.noreply.github.com>
2021-01-29 10:47:54 +0100

    update

--- React/Tutorial2/myapp/src/App.js
+++ React/Tutorial2/myapp/src/App.js
@@ -1,49 +1,49 @@
 import React, { Component } from 'react'
 import { Container, Input, Label, Button } from 'reactstrap'
-// import ChildComponent from './Components/ChildComponent'
-import Counter from './Components/Counter'
+// import Counter from './Components/Counter'
 
 class App extends Component {
   constructor(props) {
       super(props)
-      this.state = {
-          username: 'Arne',
-          password: '1234',
-          age: 12,
-      }
+      this.state = {fullName: '', age: '', email: ''};
   }
 
-  inputHandler = (e) => {
-      // console.log(e.target.value)
-      this.setState({
-          comment: e.target.value
-      })
+  handleChange = (e) => {
+    this.setState({
+        [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
   }
 
   render() {
       return (
           <div>
-              <Counter projectname="Counter Project v1.0" />
+              {/* <Counter projectname="Counter Project v1.0" /> */}
               <hr />
-              <Input type="text" className="bg-primary text-white" onChange={this.inputHandler} />
 
-              <p className="lead">
-                  {this.state.comment}
-              </p>
-              
               <Container>
-                <Label>Full Name: </Label>
-                <Input type="text" className="bg-white text-dark" onChange={this.inputHandler} />
+              <p className="lead font-weight-bold">Full Name: {this.state.fullName}</p>
+              <p className="lead font-weight-bold">Age: {this.state.age}</p>
+              <p className="lead font-weight-bold">E-mail: {this.state.email}</p>
+              <hr/>
+              <div className="bg-dark p-4 text-white">
+              <Label>Full Name: </Label>
+                <Input name="fullName" type="text" className="bg-white text-dark" onChange={this.handleChange} />
                 <Label className="mt-3">Age: </Label>
-                <Input type="number" className="bg-white text-dark" onChange={this.inputHandler} />
+                <Input name="age" type="number" className="bg-white text-dark" onChange={this.handleChange} />
                 <Label className="mt-3">E-mail: </Label>
-                <Input type="email" className="bg-white text-dark" onChange={this.inputHandler} />
-                <Button className="mt-4" color="info">Send Message</Button>
+                <Input name="email" type="email" className="bg-white text-dark" onChange={this.handleChange} />
+               
+                <Button className="mt-4" color="info" onClick={this.handleSubmit}>Send Message</Button>
+              </div>
               </Container>
           </div>
       )
   }
 }
-
 
 export default App
