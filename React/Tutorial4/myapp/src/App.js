ecbbf74c5a15d6e8cb86af256cfaf9625d5705b3
Yannick De Decker <71879638+YannickDeDecker@users.noreply.github.com>
2021-02-01 19:16:53 +0100

    update

---
+++ React/Tutorial4/myapp/src/App.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+import { Container, FormGroup, Label, Input } from 'reactstrap';
+import './App.css';
+import fetch from 'node-fetch'
+
+class WeatherApplication extends Component {
+  componentDidMount() {
+    
+}
+
+state = {
+  city: "3337389",
+  data: {
+    weather: [{}],
+    main: {},
+    wind: {}
+  },
+  imageSrc: ''
+}
+
+updateWeather= (e) => {
+  if(e.target.value === ''){
+    return
+  } 
+  
+  fetch(`http://api.openweathermap.org/data/2.5/weather?id=${e.target.value}&appid=0f353bcfac3a777a9f3aa79d952733eb`)
+  .then(response => response.json())
+  // .then(json => console.log(json))
+  .then(json => {
+    console.log(json);
+    this.setState({
+      city: e.target.value,
+      data: json,
+      imageSrc: `http://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png`
+    })
+  })
+}  
+
+render() {
+    return (
+      <div className="App bg-dark p-4 text-white">
+        <Container>
+        <h1>Weather Application</h1>
+        <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla enim enim, cursus faucibus vehicula ut, lacinia quis odio. Curabitur sagittis mi at risus consequat pharetra. In feugiat nec est a finibus. Vestibulum leo diam, cursus id ullamcorper at, consequat malesuada metus. Duis tempor faucibus diam, non euismod dolor cursus nec.</p>
+        <FormGroup>
+            <Label for="exampleSelect">Choose a city:</Label>
+             <Input onChange={this.updateWeather} type="select" name="select" id="exampleSelect">
+                <option value="">--Choose Option--</option>
+                <option value="2803138">Antwerpen</option>
+                <option value="2800931">Brugge</option>
+                <option value="3337389">Brussel</option>
+                <option value="2797671">Genk</option>
+                <option value="2797656">Gent</option>
+                <option value="2754779">Hasselt</option>
+                <option value="2794055">Kortrijk</option>
+                <option value="2792482">Leuven</option>
+                <option value="2792413">Luik</option>
+                <option value="2789787">Oostende</option>
+                <option value="2785141">Turnhout</option>
+                <option value="2784604">Vilvoorde</option>
+             </Input>
+        </FormGroup>
+        <h1>{this.state.data.name}</h1>
+        <img className="img-circle" src={this.state.imageSrc}></img>
+        <div className="bg-container">
+        <p><span>Weather:</span> {this.state.data.weather[0].main}</p>
+        <p><span>Description:</span> {this.state.data.weather[0].description}</p>
+        <p><span>Min. Temperature:</span> {Math.round(this.state.data.main.temp_min-273.15)}°C</p>
+        <p><span>Max. Temperature:</span> {Math.round(this.state.data.main.temp_max-273.15)}°C</p>
+        <p><span>Wind speed:</span> {Math.round(this.state.data.wind.speed*3.6)} km/hour</p>
+        </div>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default WeatherApplication;
